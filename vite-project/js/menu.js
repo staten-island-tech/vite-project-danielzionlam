74d85f59613147efe42e73821a360928334f08f4
danielzionlam <111651448+danielzionlam@users.noreply.github.com>
2023-01-01 00:36:42 -0500

    m

---
+++ vite-project/js/menu.js
@@ -0,0 +1,60 @@
+const animals = [
+  {
+    name: "Curious George",
+    armedanddangerous: true,
+    price: 40000,
+    img: "https://i1.sndcdn.com/artworks-t96S71fPBRmBZ1z4-drdGJw-t500x500.jpg",
+    sus: "yes",
+  },
+  {
+    name: "Ernie",
+    armedanddangerous: true,
+    price: 5,
+    img: "https://media.tenor.com/images/3751f40cc947cb78df02546ef2ae6b34/tenor.png",
+    sus: "no",
+  },
+  {
+    name: "Cookie Monster",
+    armedanddangerous: true,
+    price: 25000,
+    img: "https://pbs.twimg.com/profile_images/1471414184407560194/7wOQeSlF_400x400.jpg",
+    sus: "yes",
+  },
+  {
+    name: "Moto Moto",
+    armedanddangerous: false,
+    price: 6000,
+    img: "http://images7.memedroid.com/images/UPLOADED334/6023daf3a8523.jpeg",
+    sus: "no",
+  },
+  {
+    name: "Peppa Pig",
+    armedanddangerous: true,
+    price: 20,
+    img: "https://i1.sndcdn.com/artworks-SpxBvcTo1cDQQxdg-2S83lA-t500x500.jpg",
+    sus: "arguable",
+  },
+  {
+    name: "Boots",
+    armedanddangerous: false,
+    price: 10,
+    img: "https://i5.walmartimages.com/asr/7787e3b8-db77-478a-af01-513a8664e653_1.b2061dfb3fcd407265c9a9571d9b2908.jpeg",
+    sus: "argueable",
+  },
+  {
+    name: "Lightskin Dog",
+    armedanddangerous: false,
+    price: 0.5,
+    img: "https://pbs.twimg.com/media/FYWf5ymXEAIBZRk.jpg",
+    sus: "yes",
+  },
+  {
+    name: "Quack",
+    armedanddangerous: false,
+    price: 100000,
+    img: "https://resizing.flixster.com/VENcuXSukEf_jSuULNA_b9aA6Mw=/206x305/v2/https://flxt.tmsimg.com/assets/p2965542_e_v8_aa.jpg",
+    sus: "no",
+  },
+];
+
+export { animals };
